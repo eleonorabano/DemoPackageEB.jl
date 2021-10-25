f4bbe1b0d76b3422e70ea564e067992eb8b2342d
Documenter.jl <documenter@juliadocs.github.io>
2021-10-25 03:14:36 +0000

    build based on e33b933

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageEB","category":"page"},{"location":"#DemoPackageEB","page":"Home","title":"DemoPackageEB","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageEB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageEB]","category":"page"}]
+}
